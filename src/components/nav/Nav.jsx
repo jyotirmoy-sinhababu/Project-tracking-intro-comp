4679a2581a71a6e612e707959fe0e6bb0e9c0e70
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-11-08 17:16:54 +0530

    style added to nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 import logo from '../../assets/logo.svg';
 
 const Nav = () => {
   return (
-    <div>
+    <div className='flex justify-around p-[4%]'>
       <div>
         <img src={logo} alt='logo' />
       </div>
-      <div>
-        <div>
+      <div className='sm-hidden flex items-center gap-[59px] font-medium'>
+        <div className='flex gap-[25px]'>
           <button>PRODUCT</button>
           <button>FEATURES</button>
           <button>PRICING</button>
@@ -17,6 +18,9 @@ const Nav = () => {
         <div>
           <button>LOGIN</button>
         </div>
+      </div>
+      <div className='hidden sm-flex'>
+        <GiHamburgerMenu />
       </div>
     </div>
   );
